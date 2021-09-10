4812b479cda2e5049bb1ed4a8fb4e965336befd2
abdoadel1 <adel14606@gmail.com>
2021-09-10 19:06:17 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,241 @@
+var nameInput = document.getElementById('productName');
+var categoryInput = document.getElementById('productCategory');
+var priceInput = document.getElementById('productPrice');
+var descriptionInput = document.getElementById('productDescription');
+var tBody = document.getElementById('tbody');
+var searchInput = document.getElementById('search');
+var mainBtn = document.getElementById('mainBtn');
+var updateBtn = document.getElementById('updateBtn');
+// our local storage start
+var productList =[];
+if(localStorage.getItem('oldProduct') !=null){
+  productList = JSON.parse(localStorage.getItem('oldProduct'));
+  displayProduct();
+}
+// our local storage end
+
+// adding function
+function addProduct(){
+  var newPrice = Number(priceInput.value);
+  var singleProduct = {
+    name:nameInput.value,
+    category:categoryInput.value,
+    price:newPrice,
+    description:descriptionInput.value,
+  }
+  productList.push(singleProduct);
+  displayProduct();
+  clearForm();
+  localStorage.setItem('oldProduct', JSON.stringify(productList));
+}
+// clear function
+function clearForm(){
+  nameInput.value = "",
+  categoryInput.value = "",
+  priceInput.value = "",
+  descriptionInput.value = ""
+}
+// display function
+function displayProduct(){
+  var str ="";
+  
+  for (i=0;i<productList.length;i++){
+    str +=`<tr>
+    <td>${i}</td>
+    <td>${productList[i].name}</td>
+    <td>${productList[i].category}</td>
+    <td>${productList[i].price}</td>
+    <td>${productList[i].description}</td>
+    <td>
+      <button class="btn btn-warning" onclick="updateProduct(${i});">
+
+        <i class="fas fa-edit"></i>
+
+
+      </button>
+    </td>
+    <td>
+      <button class="btn btn-danger" onclick="deleteProduct(${i});">
+        <i class="fas fa-trash"></i>
+      </button>
+    </td>
+</tr>`
+  }
+  tBody.innerHTML = str;
+}
+// delete fuction start
+function deleteProduct(productIndex){
+  productList.splice(productIndex,1);
+  localStorage.setItem('oldProduct', JSON.stringify(productList));
+  displayProduct()
+
+}
+// search function
+function searchProduct(searchWord){
+  var str ="";
+  for(i=0;i<productList.length;i++){
+    if(productList[i].name.toLowerCase().includes(searchWord.toLowerCase())){
+      str +=`<tr>
+    <td>${i}</td>
+    <td>${productList[i].name}</td>
+    <td>${productList[i].category}</td>
+    <td>${productList[i].price}</td>
+    <td>${productList[i].description}</td>
+    <td>
+      <button class="btn btn-warning" onclick="updateProduct(${i});">
+
+        <i class="fas fa-edit"></i>
+
+
+      </button>
+    </td>
+    <td>
+      <button class="btn btn-danger" onclick="deleteProduct(${i});">
+        <i class="fas fa-trash"></i>
+      </button>
+    </td>
+</tr>`
+    }
+  }
+  tBody.innerHTML = str;
+}
+
+// update function start
+updateBtn.style.display = "none" ;
+function updateProduct(index){
+  nameInput.value = productList[index].name;
+  categoryInput.value = productList[index].category;
+  priceInput.value = productList[index].price;
+  descriptionInput.value = productList[index].description;
+  mainBtn.style.display = "none";
+  deleteProduct(index);
+  updateBtn.style.display = "inline-block" ;
+  console.log(index)
+}
+function update(index){
+  var newPrice = Number(priceInput.value);
+  var singleProduct = {
+    name:nameInput.value,
+    category:categoryInput.value,
+    price:newPrice,
+    description:descriptionInput.value,
+  }
+  productList.splice(index,0,singleProduct);
+  displayProduct();
+  clearForm();
+  localStorage.setItem('oldProduct', JSON.stringify(productList));
+  updateBtn.style.display = "none";
+  mainBtn.style.display = "inline-block";
+  console.log(index)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(localStorage.getItem('savedProducts') != null){
+//   productList = JSON.parse(localStorage.getItem('savedProducts'));
+//   displayProduct();
+// }else{
+//   productList =[];
+// }
+
+
+
+
+
+// var productList = [];
+
+// function addProduct() {
+//     var newPrice = Number(priceInput.value);
+//     var singleProduct = {
+//         productName: nameInput.value,
+//         productCategory:CategoryInput.value,
+//         productPrice:newPrice,
+//         productDescription:DescriptionInput.value,
+//     }
+    
+//     productList.push(singleProduct);
+//     displayProducts();
+//     console.log(productList);
+// }
+// function displayProducts(){
+//     var str = "";
+//     for(i=0;i<productList.length;i++){
+//         str+=`     
+
+      //<tr>
+//         <td>${i+1}</td>
+//         <td>${productList[i].productName}</td>
+//         <td>${productList[i].productCategory}</td>
+//         <td>${productList[i].productPrice}</td>
+//         <td>${productList[i].productDescription}</td>
+//         <td>
+//           <button class="btn btn-warning">
+    
+//             <i class="fas fa-edit"></i>
+    
+    
+//           </button>
+//         </td>
+//         <td>
+//           <button class="btn btn-danger">
+//             <i class="fas fa-trash"></i>
+//           </button>
+//         </td>
+//         </tr>
+//         <tr>
+         
+//         </tr>`
+    
+//     }
+//     document.getElementById('tbody').innerHTML= str;
+    
+// }
